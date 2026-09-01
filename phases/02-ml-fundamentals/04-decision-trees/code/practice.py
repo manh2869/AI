ae1bf41d16c354a92df49eaa4e1e7de0ed1e64cf
manh2869 <mank159bmt@gmail.com>
2026-09-01 23:36:08 +0700

    1/9/2026

--- phases/02-ml-fundamentals/04-decision-trees/code/practice.py
+++ phases/02-ml-fundamentals/04-decision-trees/code/practice.py
@@ -1,3 +1,5 @@
+import math
+
 # count = {}
 
 # count = {"dog": 4, "cat": 1, "fish": 2}
@@ -9,7 +11,7 @@
 # count["manh"]=2 it is push
 
 
-def gini_impurity(labels):  #gini in a node of tree 
+def gini_impurity(labels):  #  gini impurity of a node
     if len(labels) == 0:
         return 0.0
     count = {}
@@ -18,5 +20,18 @@ def gini_impurity(labels):  #gini in a node of tree
     return 1 - sum((s / len(labels)) ** 2 for s in count.values())
 
 
+def entropy(labels):
+    n = len(labels)
+    if len(labels) == 0:
+        return 0.0
+    count = {}
+    for label in labels:
+        count[label] = count.get(label, 0) + 1
+    return -sum((s / n) * math.log2(s / n) for s in count.values() if s > 0)
+
+
 labels = [1, 1, 1, 0, 0]
+labels2 = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+
 print(gini_impurity(labels))
+print(entropy(labels2))
