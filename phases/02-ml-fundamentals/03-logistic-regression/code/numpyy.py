e338db387426c46689ca252ddf0b395c5bf58898
manh2869 <mank159bmt@gmail.com>
2026-09-03 19:41:21 +0700

    3/9/2026

--- phases/02-ml-fundamentals/03-logistic-regression/code/numpyy.py
+++ phases/02-ml-fundamentals/03-logistic-regression/code/numpyy.py
@@ -6,7 +6,7 @@ import numpy as np
 # print(X.squeeze())
 # print(X.shape)
 from sklearn.datasets import make_regression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
 
 X, y = make_regression(n_samples=10, n_features=2, noise=5, random_state=42)
@@ -16,6 +16,7 @@ model = LinearRegression()
 model.fit(X, y)
 y_hat = model.predict(X)
 
+
 # print(X)
 # print(y)
 
@@ -24,7 +25,27 @@ y_hat = model.predict(X)
 # print(y_hat)
 
 
-mes=mean
+# mse = mean_squared_error(y, y_hat)
 
-result = (y - y_hat) ** 2
-print(result.mean())
+# r2 = r2_score(y, y_hat)
+# print(mse)
+# print(r2)
+# result = (y - y_hat) ** 2
+# print(result.mean())
+
+
+#                               scaler
+
+from sklearn.preprocessing import StandardScaler
+
+
+# print(y.reshape(-1, 1)) must reshape befor scaler can't scaler [  15.43422492  -24.52677522  142.00003238  132.83489946   12.13510206]
+# after reshape [[  15.43422492]
+#                 [ -24.52677522]
+#                 [ 142.00003238]
+#                 [ 132.83489946]
+#                 [  12.13510206]]
+scaler = StandardScaler()
+y_scaler = scaler.fit_transform(y.reshape(-1, 1))
+print(y)
+print(y_scaler)
