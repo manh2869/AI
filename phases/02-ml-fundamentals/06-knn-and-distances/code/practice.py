4a7c6b0ff37b2f29f6aaa9c7c068dcc508d95c32
manh2869 <mank159bmt@gmail.com>
2026-09-05 20:28:53 +0700

    5/9/2026

--- phases/02-ml-fundamentals/06-knn-and-distances/code/practice.py
+++ phases/02-ml-fundamentals/06-knn-and-distances/code/practice.py
@@ -1,11 +1,6 @@
 import numpy as np
 import math
 
-x = np.array(
-    [[150, 8], [160, 7], [140, 9], [300, 5], [320, 4], [280, 6], [155, 8], [310, 5]]
-)
-x_new = np.array([[200, 11]])
-
 
 y = ["Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Apple", "Orange"]
 
@@ -13,9 +8,17 @@ dic = {"Apple": 0, "Orange": 1}
 
 y = [dic[label] for label in y]
 
+x = np.array(
+    [[150, 8], [160, 7], [140, 9], [300, 5], [320, 4], [280, 6], [155, 8], [310, 5]]
+)
+x_new = np.array([[200, 11]])
+
 
 def euclidean_distance(x, x_new):
-    return math.sqrt(sum((x1 - x_new) ** 2 for x1 in x))
+    result = []
+    for i in x:
+        result.append(math.sqrt(sum((sum((i - x_new) ** 2)))))
+    return result
 
 
 # help(np.sort)  helppppp
